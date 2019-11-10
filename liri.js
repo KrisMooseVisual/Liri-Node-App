29fec1efef5d08884a9b552d89ba5c7685346d12
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-10 18:21:34 -0500

    added ppsuedo code and all the items needed for app

--- liri.js
+++ liri.js
@@ -14,10 +14,29 @@ var axios = require("axios");
 
 // capture imput that user puts in
 var userImput = process.argv[2];
+var userRequest = // everything index of 3 and later
 
+console.log(userImput);
 // Switch statement if userImput is "concert-this"
+    // run API call using Axios to bands-in-town-API
+    // inject user's search term in queryURL
+
+     // display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
 
 // Switch statement if userImput is "spotify-this-song"
+    // using Node Spotify package info make call to spotify API
+    // Disply to user:
+        // •ARTIST
+        // •SONGS NAME
+        // •PREVIEW LINK FROM SPOTIFY
+        // •ALBUM THAT SONG IS FROM
+    // Provide default search term if user didn't provide arguement
+
 // Switch statement if userImput is "movie-this"
-// Switch statement if userImput is "do-what-it-is"
+    
+    //
+
+// Switch statement if userImput is "do-what-it-says"
+
+// if not, then display message to the user saying "That does not compute, try again!"
 
