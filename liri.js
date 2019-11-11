11b0cdc3ee687342b83eb798f2d612becae8b8fe
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-10 20:16:03 -0500

    placed in switch statements for each catagory

--- liri.js
+++ liri.js
@@ -12,16 +12,24 @@ var axios = require("axios");
 // capture imput that user puts in
 var userImput = process.argv[2];
 var userRequest = process.argv[3]; // everything index of 3 and higher.
-
 console.log(userImput);
 
+
 // Switch statement if userImput is "concert-this"
+switch (userImput) {
+    case "concert-this":
+        concertThis();
+        break;
+
     // run API call using Axios to bands-in-town-API
     // inject user's search term in queryURL
 
      // display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
 
 // Switch statement if userImput is "spotify-this-song"
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
     // using Node Spotify package info make call to spotify API
     // Disply to user:
         // •ARTIST
@@ -31,10 +39,14 @@ console.log(userImput);
     // Provide default search term if user didn't provide arguement
 
 // Switch statement if userImput is "movie-this"
-    
+    case "movie-this":
+        movieThis();
+        break;
     //
 
 // Switch statement if userImput is "do-what-it-says"
-
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
 // if not, then display message to the user saying "That does not compute, try again!"
-
+};
