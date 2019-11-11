7c31442b557d9a0bee378154aca79d603d9ebab9
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-11 18:22:06 -0500

    added OMDB API key into queryUrl. console.log it as well

--- liri.js
+++ liri.js
@@ -9,41 +9,41 @@ var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 
 //adding moment for date of events
-var moment = require("moment");
+//var moment = require("moment");
 
 //reading and writing files
-var fs = require("fs");
+//var fs = require("fs");
 
 
 // capture imput that user puts in
-var userInput = process.argv[2];
-var userRequest = process.argv[3]; // everything index of 3 and higher.
-console.log(userInput);
-console.log(userRequest);
+// var userInput = process.argv[2];
+// var userRequest = process.argv[3]; // everything index of 3 and higher.
+// console.log(userInput);
+// console.log(userRequest);
 
 
 // Switch statement if userImput is "concert-this"
 // run API call using Axios to bands-in-town-API
 // inject user's search term in queryURL
-switch (userInput) {
-    case "concert-this":
-        concertThis();
-        break;
-    }
+// switch (userInput) {
+//     case "concert-this":
+//         concertThis();
+//         break;
+
         // inject user's search term in queryURL
         // display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
-        var moment = require('moment');
-        moment().format();
+        // var moment = require('moment');
+        // moment().format();
 
 
     // Switch statement if userImput is "spotify-this-song"
     // using Node Spotify package info make call to spotify API
-    case "spotify-this-song":
-spotifyThisSong();
-break;
+    // case "spotify-this-song":
+    //     spotifyThisSong();
+    //     break;
 
-// Disply to user:
-var spotifyList
+        // Disply to user:
+        // var spotifyList = 
     // •ARTIST
     // •SONGS NAME
     // •PREVIEW LINK FROM SPOTIFY
@@ -52,16 +52,41 @@ var spotifyList
     // Provide default search term if user didn't provide arguement
 
     // Switch statement if userImput is "movie-this"
-    case "movie-this":
-movieThis();
-break;
+    // case "movie-this":
+    //     movieThis();
+    //     break;
     //
 
     // Switch statement if userImput is "do-what-it-says"
-    case "do-what-it-says":
-doWhatItSays();
-break;
+    // case "do-what-it-says":
+    //     doWhatItSays();
+    //     break;
     // if not, then display message to the user saying "That does not compute, try again!"
-    default:
-noArguement("That does not compute. Try again!")
-};
+//     default:
+//         noArguement("That does not compute. Try again!")
+// };
+
+    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Break for Peace~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    //Usig Axios for movie-this section.
+    // storing all arguements in array
+    var nodeArgs = process.argv;
+
+    //Empty variable for movie names
+    var movieName = "";
+    
+    //For loop for node arguments 
+    for (var i = 2; i < nodeArgs.length; i++) {
+
+        if (i > 2 && i < nodeArgs.length){
+            movieName = movieName + "+" + nodeArgs[i];
+        }
+        else {
+            movieName += nodeArgs[i];
+        }
+    }
+
+    //Request through axios to OMDB API
+    var queryUrl = "http://www.omdbapi.com/?i=" + movieName + "tt3896198&apikey=4aa82962";
+    console.log(queryUrl);
+
