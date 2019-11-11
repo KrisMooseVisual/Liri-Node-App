6ec7b3d1e98dc13289e09e0a78eb82c5bf1265cf
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-11 18:07:15 -0500

    starting with OMDB first. 'concert-this' giving problems

--- liri.js
+++ liri.js
@@ -16,50 +16,52 @@ var fs = require("fs");
 
 
 // capture imput that user puts in
-var userImput = process.argv[2];
+var userInput = process.argv[2];
 var userRequest = process.argv[3]; // everything index of 3 and higher.
-console.log(userImput);
+console.log(userInput);
+console.log(userRequest);
 
 
 // Switch statement if userImput is "concert-this"
-switch (userImput) {
-
+// run API call using Axios to bands-in-town-API
+// inject user's search term in queryURL
+switch (userInput) {
     case "concert-this":
-       concertThis();
+        concertThis();
         break;
-    // run API call using Axios to bands-in-town-API
-    // inject user's search term in queryURL
-     // display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
-     var moment = require('moment');
-moment().format();
+    }
+        // inject user's search term in queryURL
+        // display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
+        var moment = require('moment');
+        moment().format();
 
 
-// Switch statement if userImput is "spotify-this-song"
-// using Node Spotify package info make call to spotify API
+    // Switch statement if userImput is "spotify-this-song"
+    // using Node Spotify package info make call to spotify API
     case "spotify-this-song":
-        spotifyThisSong();
-        break;
-    
-        // Disply to user:
-    var spotifyList
-        // •ARTIST
-        // •SONGS NAME
-        // •PREVIEW LINK FROM SPOTIFY
-        // •ALBUM THAT SONG IS FROM
+spotifyThisSong();
+break;
+
+// Disply to user:
+var spotifyList
+    // •ARTIST
+    // •SONGS NAME
+    // •PREVIEW LINK FROM SPOTIFY
+    // •ALBUM THAT SONG IS FROM
 
     // Provide default search term if user didn't provide arguement
 
-// Switch statement if userImput is "movie-this"
+    // Switch statement if userImput is "movie-this"
     case "movie-this":
-        movieThis();
-        break;
+movieThis();
+break;
     //
 
-// Switch statement if userImput is "do-what-it-says"
+    // Switch statement if userImput is "do-what-it-says"
     case "do-what-it-says":
-        doWhatItSays();
-        break;
-// if not, then display message to the user saying "That does not compute, try again!"
-default:
-        noArguement ("That does not compute. Try again!")
+doWhatItSays();
+break;
+    // if not, then display message to the user saying "That does not compute, try again!"
+    default:
+noArguement("That does not compute. Try again!")
 };
