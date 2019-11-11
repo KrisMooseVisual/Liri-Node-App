ff929cfd24ae815e4bbbe115521b3b54fbe559d2
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-11 18:36:12 -0500

    error in syntax in my else console.log. cant find error

--- liri.js
+++ liri.js
@@ -30,63 +30,84 @@ var axios = require("axios");
 //         concertThis();
 //         break;
 
-        // inject user's search term in queryURL
-        // display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
-        // var moment = require('moment');
-        // moment().format();
+// inject user's search term in queryURL
+// display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
+// var moment = require('moment');
+// moment().format();
 
 
-    // Switch statement if userImput is "spotify-this-song"
-    // using Node Spotify package info make call to spotify API
-    // case "spotify-this-song":
-    //     spotifyThisSong();
-    //     break;
+// Switch statement if userImput is "spotify-this-song"
+// using Node Spotify package info make call to spotify API
+// case "spotify-this-song":
+//     spotifyThisSong();
+//     break;
 
-        // Disply to user:
-        // var spotifyList = 
-    // •ARTIST
-    // •SONGS NAME
-    // •PREVIEW LINK FROM SPOTIFY
-    // •ALBUM THAT SONG IS FROM
+// Disply to user:
+// var spotifyList = 
+// •ARTIST
+// •SONGS NAME
+// •PREVIEW LINK FROM SPOTIFY
+// •ALBUM THAT SONG IS FROM
 
-    // Provide default search term if user didn't provide arguement
+// Provide default search term if user didn't provide arguement
 
-    // Switch statement if userImput is "movie-this"
-    // case "movie-this":
-    //     movieThis();
-    //     break;
-    //
+// Switch statement if userImput is "movie-this"
+// case "movie-this":
+//     movieThis();
+//     break;
+//
 
-    // Switch statement if userImput is "do-what-it-says"
-    // case "do-what-it-says":
-    //     doWhatItSays();
-    //     break;
-    // if not, then display message to the user saying "That does not compute, try again!"
+// Switch statement if userImput is "do-what-it-says"
+// case "do-what-it-says":
+//     doWhatItSays();
+//     break;
+// if not, then display message to the user saying "That does not compute, try again!"
 //     default:
 //         noArguement("That does not compute. Try again!")
 // };
 
-    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Break for Peace~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Break for Peace~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    //Usig Axios for movie-this section.
-    // storing all arguements in array
-    var nodeArgs = process.argv;
+//Usig Axios for movie-this section.
+// storing all arguements in array
+var nodeArgs = process.argv;
 
-    //Empty variable for movie names
-    var movieName = "";
-    
-    //For loop for node arguments 
-    for (var i = 2; i < nodeArgs.length; i++) {
+//Empty variable for movie names
+var movieName = "";
 
-        if (i > 2 && i < nodeArgs.length){
-            movieName = movieName + "+" + nodeArgs[i];
+//For loop for node arguments 
+for (var i = 2; i < nodeArgs.length; i++) {
+
+    if (i > 2 && i < nodeArgs.length) {
+        movieName = movieName + "+" + nodeArgs[i];
+    }
+    else {
+        movieName += nodeArgs[i];
+    }
+}
+
+//Request through axios to OMDB API
+var queryUrl = "http://www.omdbapi.com/?i=" + movieName + "tt3896198&apikey=4aa82962";
+console.log(queryUrl);
+
+axios.get(queryUrl).then(
+    function (response) {
+        console.log("Release Year: " + response.data.Year);
+    })
+    .catch(function (error) {
+        if (error.response) {
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+        }
+        else if (error.request); {
+            console.log(error.request);
         }
         else {
-            movieName += nodeArgs[i];
+            console.log("Error", error.message);
         }
-    }
-
-    //Request through axios to OMDB API
-    var queryUrl = "http://www.omdbapi.com/?i=" + movieName + "tt3896198&apikey=4aa82962";
-    console.log(queryUrl);
-
+        console.log(error.config);
+    });
