5c4f88a50de383c093f8661f74f4f206403d6535
Mustafa Muhammad <m.muhammad43081@gmail.com>
2019-11-12 09:08:39 -0500

    re ordred syntax. still throwing errors in response for liri app.

--- liri.js
+++ liri.js
@@ -10,81 +10,23 @@ var axios = require("axios");
 
 //adding moment for date of events
 var moment = require("moment");
+moment().format();
 
-//reading and writing files
 var fs = require("fs");
 
 
-// capture imput that user puts in
-var userInput = process.argv[2];
+// grabs input that user puts in
+var userCommand = process.argv[2];// will capture "movie-this, concert-this, 'do what it says', and 'spotify' "
 var userRequest = process.argv[3]; // everything index of 3 and higher.
-console.log(userInput);
+var userFind = ''; // will capture 3 and above
+console.log(userCommand);
 console.log(userRequest);
-
-
-// Switch statement if userImput is "concert-this"
-// run API call using Axios to bands-in-town-API
-// inject user's search term in queryURL
-// switch (userInput) {
-//     case "concert-this":
-//         concertThis();
-//         break;
-
-// inject user's search term in queryURL
-// display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
-// var moment = require('moment');
-// moment().format();
-
-
-// Switch statement if userImput is "spotify-this-song"
-// using Node Spotify package info make call to spotify API
-// case "spotify-this-song":
-//     spotifyThisSong();
-//     break;
-
-// Disply to user:
-// var spotifyList = 
-// •ARTIST
-// •SONGS NAME
-// •PREVIEW LINK FROM SPOTIFY
-// •ALBUM THAT SONG IS FROM
-
-// Provide default search term if user didn't provide arguement
-
-// Switch statement if userImput is "movie-this"
-// case "movie-this":
-//     movieThis();
-//     break;
-//
-
-// Switch statement if userImput is "do-what-it-says"
-// case "do-what-it-says":
-//     doWhatItSays();
-//     break;
-// if not, then display message to the user saying "That does not compute, try again!"
-//     default:
-//         noArguement("That does not compute. Try again!")
-// };
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Break for Peace~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 //Usig Axios for movie-this section.
 // storing all arguements in array
-var nodeArgs = process.argv;
-
+var movieName = process.argv;
 //Empty variable for movie names
 var movieName = "";
-
-//For loop for node arguments 
-for (var i = 3; i < nodeArgs.length; i++) {
-
-    if (i > 3 && i < nodeArgs.length) {
-        movieName = movieName + "+" + nodeArgs[i];
-    }
-    else {
-        movieName += nodeArgs[i];
-    }
-}
 
 //Request through axios to OMDB API
 var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
@@ -104,25 +46,85 @@ axios.get(queryUrl).then(
 
         console.log("Language: " + response.data.Language);
 
-        console.log("Plot: " + response.data.Plot)
+        console.log("Plot: " + response.data.Plot);
 
         console.log("Cast: " + response.data.Cast);
     });
 
-    .catch(function (error) {
-        if (error.response) {
-            console.log("---------------Data---------------");
-            console.log(error.response.data);
-            console.log("---------------Status---------------");
-            console.log(error.response.status);
-            console.log("---------------Status---------------");
-            console.log(error.response.headers);
+//For loop for node arguments 
+for (var i = 3; i < userRequest.length; i++) {
+
+    if (i > 3 && i < userRequest.length) {
+        userFind = userFind + "+" + userRequest[i];
+    }
+    else {
+        userFind += userRequest[i];
+    }
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Break for Peace~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Switch statement if userImput is "concert-this"
+// run API call using Axios to bands-in-town-API
+// inject user's search term in queryURL
+switch (userFind) {
+    case "concert-this":
+        if (!userFind) {
+            userFind = ("Korn");
         }
-        else if (error.request) {
-            console.log(error.request);
+        concertThis(userFind);
+        break;
+
+    // inject user's search term in queryURL
+    // display venue. location. date of event. (use moment to format date of event MM/DD/YYYY)
+
+
+    // Switch statement if userImput is "spotify-this-song"
+    // using Node Spotify package info make call to spotify API
+    case "spotify-this-song":
+        console.log("Command: " + userCommand);
+        console.log("Find: " + userFind);
+        if (!userFind) {
+            //Default song Dazed and Confused by Led Zepplin
+            userFind = "Dazed and Confused (Led Zepplin)";
         }
-        else {
-            console.log("That entry does not compute. Try again!", error.message);
+        spotifyThisSong(userFind);
+        break;
+
+    // Disply to user:
+    // var spotifyList
+    // •ARTIST
+    // •SONGS NAME
+    // •PREVIEW LINK FROM SPOTIFY
+    // •ALBUM THAT SONG IS FROM
+
+    // Switch statement if userInput is "movie-this"
+    case "movie-this":
+        console.log("Command: " + userCommand);
+        
+        console.log("Find: " + userFind)
+        // Provide default search term if user didn't provide arguement 'Black Dynomite' as default movie.
+
+        if (userCommand === "movie-this") {
+            movieThis(userFind);
         }
-        console.log(error.config);
-    });
+        else if (!userSearch) {
+            userSearch = "Black+Dynomite"
+            userMovie(userSearch);
+        }
+        break;
+
+    // Switch statement if userImput is "do-what-it-says"
+    case "do-what-it-says":
+        console.log("Command: " + userCommand);
+        console.log("Find: " + userFind);
+        console.log("Command " + userCommand);
+        doWhatItSays();
+        break;
+    // if not, then display message to the user saying "That does not compute, try again!"
+    default:
+        console.log("That does not compute. Try again!")
+};
+
+
+
